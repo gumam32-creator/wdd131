77ba11f4426641393897cdae956d99e58a9ab403
gumam32-creator <gumam32@gmail.com>
2026-07-19 09:16:01 -0300

    Menu Hamburguer
    Realizado alterações Menu Hamburguer

--- scripts/templos.js
+++ scripts/templos.js
@@ -1,8 +1,18 @@
 // Indice
 const anoAtual = document.getElementById('anoatual');
-const ultimaAtualizaco = document.getElementById('ultimaModificao')
+const ultimaAtualizaco = document.getElementById('ultimaModificao');
+const hambButton = document.getElementById('menu');
+const navigation = document.querySelector('.navigation');
 
 
 const hoje = new Date();
 anoAtual.textContent = hoje.getFullYear();
 ultimaAtualizaco.textContent = `Ultima moficação realizada em ${document.lastModified}`
+
+
+// Menu Hamburguer
+
+hambButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hambButton.classList.toggle('open')
+})
