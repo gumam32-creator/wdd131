3215a8acf30c8fc27d204e373201cf2ed629fb50
gumam32-creator <gumam32@gmail.com>
2026-07-20 00:01:14 -0300

    Realizado as alterações e criação de arquivos

---
+++ semana03/scripts/atividade-aprendizagem.js
@@ -0,0 +1,11 @@
+let nome = Juliana
+let sobrenome = Maximo 
+
+
+function nomeCompleto(nome,sobrenome) {
+    return `${nome} ${sobrenome}`
+}
+
+const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
+
+
